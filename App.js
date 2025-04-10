4bfbbe8c721625ac619d77f19799852c692dfb22
Devi-Satya <devisatya1999@gmail.com>
2025-04-10 10:32:51 +0530

    day 2

--- App.js
+++ App.js
@@ -1,7 +1,8 @@
 // const heading= React.createElement("h1",{id:"heading",xyz:"abc"},"hello world from react!");
 // const root= ReactDOM.createRoot(document.getElementById("root"));
 // root.render(heading);
-
-const heading= React.createElement("div",{id:"parent"},[React.createElement("div",{id:"child"},React.createElement("h1",{},"hello from h1 tag")),React.createElement("div",{id:"child2"},React.createElement("h2",{},"hello from h2 tag"))])
+import React from "react";
+import ReactDOM from "react-dom/client"
+const heading= React.createElement("div",{id:"parent"},[React.createElement("div",{id:"child",key:"child1"},React.createElement("h1",{},"hello from h1 tag")),React.createElement("div",{id:"child2", key:"child2"},React.createElement("h2",{},"hello from h2 tag"))])
 const root= ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);
